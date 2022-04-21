0b1e981e7fe959444672bcc8e7a75b1c818957cb
akashgreninja <akashuhulekal@gmail.com>
2022-04-21 21:58:21 +0530

    yeyey

--- app.js
+++ app.js
@@ -1,29 +1,34 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
-const bodyparser=require("body-parser")
-const alert = require('alert'); 
+const bodyparser = require("body-parser");
+const alert = require("alert");
 
 const internal = require("stream");
 const res = require("express/lib/response");
 
-mongoose.connect(process.env.DATABASE || "mongodb+srv://Akashgreninja:Pokemonprimape%4013@cluster0.bdfqg.mongodb.net/ContactDance");
+// databaselocal=mongodb://localhost/ContactDance
+mongoose.connect(
+  process.env.DATABASE ||
+    "mongodb+srv://Akashgreninja:Pokemonprimape%4013@cluster0.bdfqg.mongodb.net/ContactDance"
+);
+
+// mongoose.connect("mongodb://localhost/ContactDance",{useNewUrlParser :true})
 
 const app = express();
 const port = process.env.PORT || 5000
+// const port = 8000;
 
 //schemas
 
-const ContactSchema = new mongoose.Schema({
+var ContactSchema = new mongoose.Schema({
   name: String,
   phone: String,
   email: String,
   address: String,
-  Concern: String,
+  concern: String,
 });
 const Contact = mongoose.model("Contact", ContactSchema);
-
-
 
 app.use("/static", express.static("static"));
 app.use(express.urlencoded());
@@ -36,28 +41,20 @@ app.get("/", (req, res) => {
 });
 
 app.get("/contact", (req, res) => {
-    
   res.status(200).render("contact.pug");
-
 });
 
 app.post("/contact", (req, res) => {
-    var Data=new Contact(req.body)
-    Data.save().then(()=>{
-        res.status(200).render("contact.pug")
-        
-
-    }).catch(()=>{
-        res.status(400).send("There has been some error try again")
-       
+  var Data = new Contact(req.body);
+  Data.save()
+    .then(() => {
+      // res.status(200).render("contact.pug")
+      res.status(200).send("saved");
     })
-  
-
-   
-  });
-
-
-//
+    .catch(() => {
+      res.status(400).send("failed");
+    });
+});
 
 app.listen(port, () => {
   console.log("started woah");
